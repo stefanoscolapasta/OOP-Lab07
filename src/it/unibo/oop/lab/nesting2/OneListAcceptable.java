5f0e08edf81c61509d292e4e25c3f935ab5180b6
StefanoScolari <stefano.scolari@studio.unibo.it>
2020-11-09 17:54:48 +0100

    finished also 'nesting 2'

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,18 +1,46 @@
 package it.unibo.oop.lab.nesting2;
-
-import java.util.ArrayList;
-
+import java.util.Iterator;
 import java.util.List;
 
 public class OneListAcceptable<T> implements Acceptable<T> {
 
-	@Override
-	public Acceptor<T> acceptor() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private final List<T> list;
 	
 	public OneListAcceptable(List<T> list) {
+		this.list = list;
+	}
+	
+	@Override
+	public Acceptor<T> acceptor() {
+		final Iterator<T> iterator = this.list.iterator();
+		
+		return new Acceptor<T>() {
+
+			@Override
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				try {
+					if(newElement.equals(iterator.next())) {
+						return;
+					}
+				}catch(ElementNotAcceptedException e) {
+					System.out.println(e.getMessage());
+				}
+				throw new ElementNotAcceptedException(newElement);
+			}
+
+			@Override
+			public void end() throws EndNotAcceptedException {
+				try {
+                    if (!iterator.hasNext()) {
+                        return;
+                    }
+                } catch (EndNotAcceptedException e) {
+                    System.out.println(e.getMessage());
+                }
+                throw new EndNotAcceptedException();	
+			}
+			
+		};
 		
 	}
 	
